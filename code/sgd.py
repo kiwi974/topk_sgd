1e32d64574f77b586d8cfce6c71b492d294a099c
kiwi974 <attachew974@gmail.com>
2018-05-09 18:47:21 +0200

    distributed, centralized SGD

---
+++ code/sgd.py
@@ -0,0 +1,203 @@
+import random
+import sparseToolsDict as std
+import math
+#import matplotlib.pyplot as plt
+
+
+
+########################################################################
+# Generation of data to test stochastic gradient descent. One can
+# visualize the data : remove the comments on the import of matplotlib
+# (line 2) and one the plot (lines 102 to 105). We generate points in
+# the square [0,10]x[0,10], the hyperplan is y = 10 - x.
+
+# Input :
+#       -nbData : number of generated data.
+
+# Output :
+#       -trainingSet : a set of tuples (label,example) which is a
+#        training. The format of this set is
+#        List(label : Int, example : List(float)) -> label = +1 or -1.
+########################################################################
+
+
+# d is the double of the distance of each point in the square to the
+# separator hyperplan.
+d = 2
+
+# u is a hyperplan's orthogonal vector.
+u = {1:1,2:1}
+
+def generateData(nbData):
+
+    # A and B denote each a different class, respectively associated
+    # to the labels 1 and -1.
+    A = []
+    B = []
+
+    # Number of examples we kept for our training set.
+    nbExamples = 0
+
+    # Number of data we rejected because they are not at the good
+    # distance of the hyperplan.
+    nbRejeted = 0
+
+    absA, absB = [], []
+    ordA, ordB = [], []
+
+    cardA = 0
+    cardB = 0
+
+    while (nbExamples < nbData):
+        a = random.randint(0,50)/10
+        b = random.randint(0,50)/10
+        sign = random.random()
+        if (sign >= 0.5):
+            a = -a;
+            b = -b;
+        genvect = {1:a,2:b}
+        dist = abs((std.sparse_dot(u,genvect))/(math.sqrt(std.sparse_dot(u,u))))
+        valide = (d==0) or ((d!=0) & (dist >= d))
+        if (b > -a) & valide:
+            A.append({-1:1,1:a,2:b})
+            absA.append(a)
+            ordA.append(b)
+            nbExamples += 1
+            cardA += 1
+        elif (b < -a) & valide:
+            B.append(({-1:-1,1:a,2:b}))
+            absB.append(a)
+            ordB.append(b)
+            nbExamples += 1
+            cardB += 1
+        else:
+            nbRejeted += 1
+
+    #plt.scatter(absA,ordA,s=10,c='r',marker='*')
+    #plt.scatter(absB,ordB,s=10,c='b',marker='o')
+    #plt.plot([-5,5],[5,-5],'orange')
+    #plt.show()
+
+    trainingSet = A+B
+
+    return trainingSet,absA,ordA,absB,ordB
+
+
+
+generateData(2000)
+
+
+
+########################################################################
+# Sample a set in a subset of numSamples elements.
+# Input :
+#       -set : the set to sample.
+#       -numSamples : the number of elements of set that we want in
+#        the subset.
+# Output :
+#       -dataSample : the subset (sample of set).
+#       -sampleSize : the size of the sample set (which is equal to
+#        numSamples.
+########################################################################
+
+
+def sample(set,numSamples):
+    n = len(set)
+    dataSample = []
+    cpyData = list(set)
+
+    # Sample the data set on which complete the learning phase
+    sampleSize = 0
+    while ((sampleSize < numSamples) and (sampleSize < n)):
+        s = random.randint(0, n - sampleSize - 1)
+        dataSample.append(cpyData[s])
+        del cpyData[s]
+        sampleSize += 1
+
+    return dataSample, sampleSize
+
+
+
+
+########################################################################
+# Computation of the error given by the SVM on a sample.
+# Input :
+#       -w : the vector of parameters.
+#       -l : the depreciation factor of the SVM.
+#       -sample : the training set.
+#       -sampleSize : number of examples in sample, i.e. the size of
+#        this set.
+# Output :
+#       -cost : the cost computed on sample.
+########################################################################
+
+def error(w,l,sample,sampleSize,hypPlace):
+    norm =  l*std.sparse_dot(w,w)
+    sum = 0
+    for i in range(sampleSize):
+        label = sample[i].get(-1,0)
+        example = std.take_out_label(sample[i])
+        sum += max(0,1-label*(std.sparse_dot(w,example)))
+    cost = norm + sum
+    return cost
+
+
+
+########################################################################
+# Computation of the derivation of the error on a sample.
+# Input :
+#       -w : the vector of parameters.
+#       -l : the depreciation factor of the SVM.
+#       -sample : the training set.
+#       -sampleSize : number of examples in sample, i.e. the size of
+#        this set.
+# Output :
+#       -dcost : the value of the derivative of the cost computed on
+#        sample.
+########################################################################
+
+def der_error(w,l,sample,sampleSize,hypPlace):
+    d = std.sparse_mult(l, w)
+    sum = {}
+    for i in range(sampleSize):
+        label = sample[i].get(-1,0)
+        example = std.take_out_label(sample[i])
+        if (label*(std.sparse_dot(w,example)) <= 1):
+            sum = std.sparse_vsum(sum,std.sparse_mult(label,example))
+    dcost = std.sparse_vsum(d,sum)
+    return dcost
+
+
+
+
+
+########################################################################
+# Stochastic gradient descent.
+# Input : -data : the data set to sample and on which learn.
+#         -w : the current vector of parameters.
+#         -numSamples : the number of samples we want for the learning.
+#         -step : departure step of the gradient descent.
+#         -l : depreciation factor of the SVM.
+# Output : -w : the vector of parameters according to a random sample
+#               of data.
+########################################################################
+
+
+
+def descent(data,w,numSamples,step,l,hypPlace):
+
+    # Sample of the data set.
+    dataSample,sampleSize = sample(data,numSamples)
+
+    # Derivative of the cost evaluated on dataSample.
+    d = der_error(w,l,dataSample,sampleSize,hypPlace)
+
+    # Modification of the parameter vector w.
+    w = std.sparse_vsous(w,std.sparse_mult(step,d))
+
+    return w
+
+
+
+
+
---
+++ code/sparseToolsDict.py
@@ -0,0 +1,241 @@
+########################################################################
+#       Sparse version of the library tools with dictionnaries.        #
+########################################################################
+
+import math
+
+
+
+########################################################################
+# Different operations that are usefull to write the stochastic gradient
+# descent algorithm and others.
+########################################################################
+
+# Compute the scalar product between to vectors spVec1 and spVec2.
+
+
+def sparse_dot(spVec1, spVec2):
+    return sum([val * spVec2.get(key, 0) for key, val in spVec1.items() if (key != -1)])
+
+
+# Each component of vect is modified by func.
+def sparse_map(func, spVec):
+    return {key: (val if key == -1 else func(val)) for key, val in spVec.items()}
+
+
+# Compute the sum, componentwise between u and v.
+def sparse_vsum(spVec1, spVec2):
+    similar_keys = set(spVec1.keys()) & set(spVec2.keys())
+    summ = {k: spVec1[k] + spVec2[k] for k in similar_keys}
+
+    sp1_only_keys = set(spVec1.keys()) - similar_keys
+    sp1 = {k: spVec1[k] for k in sp1_only_keys}
+
+    sp2_only_keys = set(spVec2.keys()) - similar_keys
+    sp2 = {k: spVec2[k] for k in sp2_only_keys}
+
+    summ.update(sp1)
+    summ.update(sp2)
+
+    return summ
+
+
+# Compute the substraction spVec1-spVec2 element-wise as vsum(u,mutl(-1,v))
+def sparse_vsous(spVec1, spVec2):
+    def opp(x):
+        return -x
+    return sparse_vsum(spVec1, sparse_map(opp, spVec2))
+
+
+# u is divided by v componentwise
+def sparse_vdiv(spVec1, spVec2):
+    if (len(spVec1) != len(spVec2)):
+        print('You try to use sparse_vdiv with vectors of different sizes.')
+    else:
+        similar_keys = set(spVec1.keys()) & set(spVec2.keys())
+        div = {k: (spVec1[k] / spVec2[k]) for k in similar_keys}
+        return div
+
+
+# each component of u is multiplied by a
+def sparse_mult(a,spVec1):
+    multVec = {}
+    for key, value in spVec1.items():
+        if (key != -1):
+            multVec[key] = a*value
+        else:
+            multVec[key] = value
+    return multVec
+
+
+# Retrieve the label of a dictionary if it exists
+
+def take_out_label(spVec):
+    r = dict(spVec)
+    try:
+        del r[-1]
+    except KeyError:
+        pass
+    return r
+
+# Retrieve the data with key hypPlace if it exists
+
+def take_out(spVec,hypPlace):
+    r = dict(spVec)
+    try:
+        del r[hypPlace]
+    except KeyError:
+        pass
+    return r
+
+
+# Merge all the sparse vectors in a list.
+def merge(vectors,nbClients):
+    vmoy = {}
+    for spVec in vectors:
+        vmoy = sparse_vsum(vmoy, spVec)
+    vmoy = sparse_mult(1. / nbClients, vmoy)
+    return vmoy
+
+
+####################################################################
+# Each element of the training set is a list of the form :
+# List(label : int, example : List(float). In order to send and
+# receive this data with the simplest service of gRPC (and this way
+# avoid serialization), we need to convert this data in text and
+# vice-versa. Each element of vectors are separated by '<->', a label
+# and its example are separated bu '<|>' and at least two elements
+# of the training set are separated by '<<->>'.
+# Thus, we have to different string format :
+#       -for a vector : [1,3,2,9] <=> '1<->3<->2<->9'.
+#       -for a data set : [[1,[4,3,5]],[-1,[8,9,4]]] <=>
+#                         '1<|>4<->3<->5<<->>-1<|>8<->9<->4'.
+####################################################################
+
+# Convert a dictionnary into a string.
+
+def dict2str(dict):
+    if (type(dict) != str):
+        txt = ""
+        for key, value in dict.items():
+            txt += str(key)+":"+str(value)+"<->"
+        return txt[0:-3]
+    else:
+        return dict
+
+# Convert a string vector into a dictionnary
+
+def str2dict(s):
+    v = s.split("<->")
+    dict = {}
+    for e in v:
+        kv = e.split(":")
+        dict[float(kv[0])] = float(kv[1])
+    return dict
+
+
+
+# Convert a data set (list of dictionaries) to a
+
+
+def datadict2Sstr(data):
+    dataStr =  ""
+    for d in data:
+        label = d.get(-1,0)
+        example = take_out_label(d)
+        dstr = str(label) + "<|>" + dict2str(example)
+        dataStr += dstr + "<<->>"
+    return dataStr[0:-5]
+
+# Convert a data string into a data set (lists)
+
+def str2datadict(strData):
+    frame = []
+    datastr = strData.split("<<->>")
+    for dstr in datastr:
+        lab_ex = dstr.split("<|>")
+        label = float(lab_ex[0])
+        dict = str2dict(lab_ex[1])
+        dict[-1] = label
+        frame.append(dict)
+    return frame
+
+
+
+
+
+
+
+
+
+
+
+
+####################################################################
+# Treat the data : normalise and center each example. This way,
+# examples with huge norms don't have more importance than the
+# others.
+####################################################################
+
+
+
+# Process the treatment on the set data.
+
+def dataPreprocessing(data,hypPlace):
+
+    n = len(data)
+
+    # Computation of the average for each variable in the example
+    moy = {}
+
+    for k in range(n):
+        example = take_out_label(data[k])
+        moy = sparse_vsum(moy,example)
+
+    def div(x):
+        return float(x)/n
+
+    moy = sparse_map(div,moy)
+
+
+    #print('')
+    #print("Le vecteur des moyennes est : " +str(moy))
+
+
+
+    # Computation of the deviation
+    sigma = {}
+
+    for k in range(n):
+
+        example = take_out_label(data[k])
+        dictDiff = sparse_vsous(example,moy)
+
+        def square(x):
+            return x*x
+
+        dictSquare = sparse_map(square,dictDiff)
+
+        sigma = sparse_vsum(dictSquare,sigma)
+
+
+    def sig(x):
+        return math.sqrt((1./n)*x)
+
+    sigma = sparse_map(sig,sigma)
+
+    #print("Le vecteur des ecarts-types est : " + str(sigma))
+    #print('')
+
+
+    for k in range(n):
+        label = data[k].get(-1,0)
+        example = take_out_label(data[k])
+        sous = sparse_vsous(example,moy)
+        treatedEx = sparse_vdiv(sous,sigma)
+        treatedEx[-1] = label
+        treatedEx[hypPlace] = -1
+        data[k] = treatedEx
+
+    return data
+
